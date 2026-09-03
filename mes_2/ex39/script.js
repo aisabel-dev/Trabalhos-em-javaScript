1194c4f26eb02b21608d0f0a654d9cc5e4e07b69
Anderson Isabel <andersonandreisabel@gmail.com>
2026-09-04 00:06:45 +0100

    atualiz

--- mes_2/ex39/script.js
+++ mes_2/ex39/script.js
@@ -2,14 +2,26 @@ async function buscarMoney(moeda){
 
 	try{
 
-		const resposta = await fetch (`https://economia.awesomeapi.com.br/json/last/${moeda}`)
-		const dados = await resposta.json()
-		
-		console.log(dados);
+		const resposta = await fetch(`https://economia.awesomeapi.com.br/json/last/${moeda}`)
+		const dados = await resposta.json()	
+
+
+		return {
+
+			dolar: dados.USDBRL.name,
+			usdcotacao: dados.USDBRL.bid,
+			euro:dados.EURBRL.name,
+			eurocotacao: dados.EURBRL.bid,
+			bitcon: dados.BTCBRL.name,
+			btccotacao: dados.BTCBRL.bid
+			}
 	}catch(erro){
 
 		console.log("Tente novamente!")
 	}
 }
 
-buscarMoney("ao")
+buscarMoney("USD-BRL,EUR-BRL,BTC-BRL").then(function(resultado){
+
+	console.log(resultado)
+})
